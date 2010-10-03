c2ebf9077d06ee9bf7927ba54347e3be8846fca9
Jeff Wallace <jeff@tjwallace.ca>
2010-10-02 23:05:41 -0400

    Adding establishment model.

---
+++ app/models/establishment.rb
@@ -0,0 +1,3 @@
+class Establishment < ActiveRecord::Base
+  belongs_to :type
+end
---
+++ db/migrate/20101003030147_create_establishments.rb
@@ -0,0 +1,17 @@
+class CreateEstablishments < ActiveRecord::Migration
+  def self.up
+    create_table :establishments do |t|
+      t.string :name
+      t.string :address
+      t.belongs_to :type
+
+      t.timestamps
+    end
+
+    add_index :establishments, :type_id
+  end
+
+  def self.down
+    drop_table :establishments
+  end
+end
