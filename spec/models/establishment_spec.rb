03bf10c2acf24c50823e52dbece1e1142ee7ea5e
Jeff Wallace <jeff@tjwallace.ca>
2010-10-16 14:26:13 -0400

    Improve specs.

--- spec/models/establishment_spec.rb
+++ spec/models/establishment_spec.rb
@@ -5,13 +5,14 @@ describe Establishment do
   it { should belong_to(:type) }
   it { should have_many(:infractions) }
 
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:address) }
-  it { should validate_presence_of(:owner) }
-  it { should validate_presence_of(:type) }
+  %w(name address owner type).each do |attr|
+    it { should validate_presence_of(attr) }
+  end
 
   it "should recognize when it's geocoded" do
-    e = Establishment.new :latitude => 1.0, :longitude => 1.0
+    e = Establishment.new
+    e.should_not be_geocoded
+    e.attributes = { :latitude => 1.0, :longitude => 1.0 }
     e.should be_geocoded
   end
 end
--- spec/models/infraction_spec.rb
+++ spec/models/infraction_spec.rb
@@ -3,14 +3,16 @@ require 'spec_helper'
 describe Infraction do
   it { should belong_to(:establishment) }
 
-  [ :establishment, :amount, :infraction_date, :judgment_date ].each do |attr|
+  %w(establishment amount infraction_date judgment_date).each do |attr|
     it { should validate_presence_of(attr) }
   end
   it { should validate_numericality_of(:amount) }
-  [ -1, 1.1 ].each do |value|
+  [-1, 1.1].each do |value|
     it { should_not allow_value(value).for(:amount) }
   end
-  it { should allow_value(100).for(:amount) }
+  [1, 100, 1000].each do |value|
+    it { should allow_value(value).for(:amount) }
+  end
 
   it "should translate description" do
     i = Infraction.create :description => 'english', :amount => 1
