b9f2ecc6b6698cc1cb81b95c9b4cd4ee71b4dad8
Jeff Wallace <jeff@tjwallace.ca>
2010-10-31 01:04:17 -0400

    Cleanup specs.

--- spec/models/establishment_spec.rb
+++ spec/models/establishment_spec.rb
@@ -2,7 +2,7 @@
 require 'spec_helper'
 
 describe Establishment do
-  before(:each) { Factory.create(:establishment) }
+  subject { Factory.create :establishment }
 
   it { should belong_to(:type) }
   it { should have_many(:infractions).dependent(:destroy) }
@@ -13,41 +13,36 @@ describe Establishment do
   it { should validate_uniqueness_of(:name) }
 
   describe "geocode" do
-    before(:each) do
-      @e = Factory.build :establishment
-    end
+    subject { Factory.build :establishment }
 
     it "should geocode a good address" do
-      @e.address = "839 Rue Sherbrooke Ouest, Montréal, Québec" # McGill
-      @e.save
-      # for rspec 2.0.1
-      #@e.latitude.should be_within(0.00001).of(45.5039069)
-      #@e.longitude.should be_within(0.00001).of(-73.5745631)
-      @e.latitude.should be_close(45.5039060, 0.00001)
-      @e.longitude.should be_close(-73.5745631, 0.00001)
-      @e.street.should == "839 Rue Sherbrooke Ouest"
-      @e.locality.should == "Montréal"
-      @e.region.should == "QC"
-      @e.postal_code.should == "H3A 2K6"
-      @e.country.should == "CA"
+      subject.update_attribute :address, "839 Rue Sherbrooke Ouest, Montréal, Québec"
+
+      #subject.latitude.should be_within(0.00001).of(45.5039069)
+      #subject.longitude.should be_within(0.00001).of(-73.5745631)
+      subject.latitude.should be_close(45.5039060, 0.00001)
+      subject.longitude.should be_close(-73.5745631, 0.00001)
+
+      subject.street.should == "839 Rue Sherbrooke Ouest"
+      subject.locality.should == "Montréal"
+      subject.region.should == "QC"
+      subject.postal_code.should == "H3A 2K6"
+      subject.country.should == "CA"
     end
 
     it "should not geocode a bad address" do
-      @e.address = "try and geocode this google!"
-      @e.save
-      @e.should_not be_geocoded
+      subject.update_attribute :address, "try and geocode this google!"
+      subject.should_not be_geocoded
     end
 
     it "should recognize when it's geocoded" do
-      @e.should_not be_geocoded
-      @e.attributes = { :latitude => 1.0, :longitude => 1.0 }
-      @e.should be_geocoded
+      subject.should_not be_geocoded
+      subject.attributes = { :latitude => 1.0, :longitude => 1.0 }
+      subject.should be_geocoded
     end
   end
 
   describe "#infractions_amount" do
-    subject { Factory.create :establishment }
-
     it "returns 0 for no infractions" do
       subject.infractions_amount.should == 0
     end
