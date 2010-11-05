26869175abfb8f98ef5e453b35205cfdbeed2410
Jeff Wallace <jeff@tjwallace.ca>
2010-11-05 00:42:41 -0400

    Tuning up establishments#index. Added sortable columns.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def javascript(*args)
     content_for :head, javascript_include_tag(*args)
   end
+
+  # railscasts episode 228
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    css_class = (column == sort_column) ? "current #{sort_direction}" : nil
+    direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
+    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+  end
 end
