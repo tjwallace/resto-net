6647c9fc5170fb7450c020dfabfd36261530dc60
Jeff Wallace <jeff@tjwallace.ca>
2010-12-11 16:48:15 -0500

    Add search param for owners API.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,13 +1,14 @@
 class OwnersController < ApplicationController
-  caches_page :index, :show
+  caches_action :index, :cache_path => Proc.new{ |c| c.params }
+  caches_page :show
+
   respond_to :json, :xml
 
   def index
-    respond_with Owner.all
+    respond_with Owner.search(params[:search])
   end
 
   def show
     respond_with Owner.find(params[:id])
   end
-
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -3,4 +3,8 @@ class Owner < ActiveRecord::Base
   has_many :establishments, :through => :infractions, :uniq => true
 
   validates_presence_of :name
+
+  def self.search(search)
+    search ? where("name LIKE ?", "%#{search}%") : scoped
+  end
 end
