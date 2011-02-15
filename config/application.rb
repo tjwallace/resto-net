961b7e177a47cc3c2754d9c7a23fbb09b8a069d9
Jeff Wallace <jeff@evolvingweb.ca>
2011-02-15 13:00:34 -0500

    Aggregate javascript files.

--- config/application.rb
+++ config/application.rb
@@ -32,10 +32,7 @@ module InspectionsOuvert
 
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
-    config.action_view.javascript_expansions[:defaults] = %w(
-      https://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js
-      rails
-    )
+    config.action_view.javascript_expansions[:defaults] = %w(rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
